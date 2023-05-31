e4c9fcae9dd945f1e83a3ee659c1e1c04b5101b3
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-05-31 08:44:59 -0500

    added:     - feature's route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './feature/components/container/login'
 
@@ -9,7 +10,12 @@ function App() {
 
   return (
     <>
-     <Login></Login>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} /> {/* Login como página principal */}
+          <Route path="/registro" element={<RegistrationAuth />} /> {/* Ruta para el registro */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
