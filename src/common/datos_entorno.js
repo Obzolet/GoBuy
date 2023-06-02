63e8c38cf31efdb4d3ed54a87d04d40a702bb8b2
Shelcean <sstivenalarcon@gmail.com>
2023-06-02 18:13:17 -0500

    update:  se agrego mas datos

--- src/common/datos_entorno.js
+++ src/common/datos_entorno.js
@@ -3,7 +3,7 @@ export const producto =[
         id:0,
         nombre: "cicla",
         precio: 100000,
-        descripcion: "es una cicla de color amarilla",
+        descripcion: "consequuntur aperiam minima sed dicta odit numquam sapiente quam eum, architecto",
         imagen: "img/img5.png",
         quantity: 6
     },
@@ -11,7 +11,7 @@ export const producto =[
         id:1,
         nombre: "cicla",
         precio: 110000,
-        descripcion: "es una cicla de color azul",
+        descripcion: "adipisicing elit. Deserunt veniam eius aspernatur ad consequuntur aperiam minima sed dicta odit numquam sapiente quam eum, architecto",
         imagen: "img/img5.png",
         quantity: 6
         },
@@ -19,7 +19,7 @@ export const producto =[
         id:2,
         nombre: "cicla",
         precio: 120000,
-        descripcion: "es una cicla de color verde",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt veniam eius aspernatur ad consequuntur",
         imagen: "img/img5.png",
         quantity: 6
     },
@@ -27,7 +27,34 @@ export const producto =[
         id:3,
         nombre: "cicla",
         precio: 130000,
-        descripcion: "es una cicla de color rosado",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sapiente!",
+        imagen: "img/img5.png",
+        quantity: 6
+    }
+    ,
+    {
+        id:4,
+        nombre: "cicla",
+        precio: 170000,
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sapiente!",
+        imagen: "img/img5.png",
+        quantity: 6
+    }
+    ,
+    {
+        id:5,
+        nombre: "cicla",
+        precio: 180000,
+        descripcion: "adipisicing elit. Deserunt veniam eius aspernatur ad consequuntur aperiam minima sed dicta odit numquam sapiente quam eum, architecto",
+        imagen: "img/img5.png",
+        quantity: 6
+    }
+    ,
+    {
+        id:6,
+        nombre: "cicla",
+        precio: 130000,
+        descripcion: "consequuntur aperiam minima sed dicta odit numquam sapiente quam eum, architecto",
         imagen: "img/img5.png",
         quantity: 6
     }
